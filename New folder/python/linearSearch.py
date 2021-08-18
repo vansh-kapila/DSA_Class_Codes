56dec7b79352bf77b0c912a75c75e54e4c516383
vansh-kapila <72248500+vansh-kapila@users.noreply.github.com>
2021-08-18 13:05:43 +0530

    Delete New folder directory

--- New folder/python/linearSearch.py
+++
@@ -1,9 +0,0 @@
-if __name__=="__main__":
-    n = int(input("Enter the size of the array -> "))
-    array=list(map(int,input("Enter the elements -> ").strip().split()))
-    target=int(input("Enter the target value -> "))
-    for i in range(n):
-        if array[i]==target:
-            print("Element found at",i)
-            quit()
-    print("Element not found")
